bbdb8b2cedb77f497d521b22662bde31770835d5
John <elementtest8@gmail.com>
2023-12-21 13:24:12 +0800

    74859724895389

--- clpboard.sh
+++ clpboard.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #copy contents of file specified to system clipboard using xclip
+ls
 read -p "what file do you want to copy to clipboard?: " input
 
 xclip -sel clip < $input
---
+++ ri.sh
@@ -0,0 +1,5 @@
+read -p "let your thoughts die here:: " input
+
+echo "$input" >> ~/.3emac.rt
+echo "$input" >> ~/.3emac.rtbackup
+clear
