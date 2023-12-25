d08ba1ba04331a82372b7aae093ef59ebdce8e53
John <elementtest8@gmail.com>
2023-12-25 17:04:56 +0800

    erase nvim

--- erase-nvim.sh
+++ erase-nvim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # WILL REMOVE ALL NVIM CONFIG FILES 
 # AND GO BACK TO VANILLA NVIM
-rm -rf ~/.config/nvim
+# rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.cache/nvim
-cd ~/.config
-mkdir nvim
+# cd ~/.config
+# mkdir nvim
