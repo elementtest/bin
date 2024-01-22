1449b819fea90be3a6470c63de2de5d99aa607c9
John <elementtest8@gmail.com>
2024-01-23 07:07:39 +0800

    16889153802470

--- htmk.sh
+++ htmk.sh
@@ -2,14 +2,19 @@
 #script to make a new directory with index, style.css and script.js
 read -p "Enter name of directory?  will generate index, css and js inside: then cd into and open index.html  " dirname
 
-mkdir "$dirname"
+mkdir "$dirname" || { echo "Failed to create directory"; exit 1; }
 
-cd "$dirname"
+cd "$dirname" || { echo "Failed to change directory"; exit 1; }
 
 touch index.html
-touch style.css
+touch styles.css
 touch script.js
 
+cp ../boilerplate.html index.html  || { echo "failed to copy boilerplate.html"; exit 1;}
+
+cp ../stylesboilerplate.css styles.css || { echo "failed to copy stylesboilerplate.css"; exit 1; }
+
+nvim index.html
 
 
 
