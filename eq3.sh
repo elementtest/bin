78365a38dfc6c17fe953670d1e3167fd8d4c032a
elementtest <elementtest8@gmail.com>
2024-04-02 07:45:44 +0800

    11028830317375

--- eq3.sh
+++ eq3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # if you don't have the script's cwd set, you get XML errors
-export WINEPREFIX=$HOME/.wine/
+export WINEPREFIX="$HOME/.wine/"
 cd ~/.wine/drive_c/"Program Files (x86)"/Sony/EverQuest/
 # 2>/dev/null dumps error messages generated from script into trash
 # error messages dont do anything.  Game should work fine
