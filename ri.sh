8c5026d5bcd924ee77dc5112a505de88fa6678a8
elementtest <elementtest8@gmail.com>
2024-06-05 17:49:55 +0800

    4821122254829

--- ri.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "let your thoughts die here:: " input
-
-echo "$input" >> ~/.3emac.rt
-#disable backup
-#echo "$input" >> ~/.3emac.rtbackup
-#clear entire file
-#echo "" > ~/.3emac.rt
-clear
