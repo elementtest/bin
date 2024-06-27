162cf6ae885e2406f82aae2c6baab4a5830261da
elementtest <elementtest8@gmail.com>
2024-06-27 20:12:40 +0800

    update NVIM-CREATE-JS-FILE 2399893622729

---
+++ NVIM-CREATE-JS-FILE.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+#script to make files and add codewars solutions
+
+read -p "file name ? " filename
+touch "$filename"
+read -p "instructions of the challenge? " shuoming
+echo "//$shuoming" >> "$filename"
+# use xclip to take copy / paste register from system clipboard and append it to file
+xclip -selection clipboard -o >> "$filename"
+cat $filename
