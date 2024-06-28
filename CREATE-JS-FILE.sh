518eaadbf20010975378850f616765a23ef1252a
elementtest <elementtest8@gmail.com>
2024-06-28 22:52:50 +0800

    30510460423208

--- CREATE-JS-FILE.sh
+++ CREATE-JS-FILE.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 #script to make files and add codewars solutions
 
+ls
 read -p "file name ? " filename
 touch "$filename.js"
 read -p "instructions of the challenge? " shuoming
 echo "//$shuoming" >> "$filename.js"
 # use xclip to take copy / paste register from system clipboard and append it to file
 xclip -selection clipboard -o >> "$filename.js"
-cat $filename
+cat $filename.js
