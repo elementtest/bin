f7cc52be80a3ee47b62e998b2273ba11a4b1bd90
elementtest <elementtest8@gmail.com>
2024-07-14 19:23:41 +0800

    30828199722949

--- htmk.sh
+++ htmk.sh
@@ -10,10 +10,6 @@ touch index.html
 touch styles.css
 touch script.js
 
-cp ../boilerplate.html index.html  || { echo "failed to copy boilerplate.html"; exit 1;}
-
-cp ../stylesboilerplate.css styles.css || { echo "failed to copy stylesboilerplate.css"; exit 1; }
-
 nvim *
 
 
