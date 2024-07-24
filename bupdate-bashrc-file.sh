f37338861151c4b0e6179a0c0b63ab19294fdc13
elementtest <elementtest8@gmail.com>
2024-07-24 12:53:26 +0800

    2667316902809

--- bupdate-bashrc-file.sh
+++
@@ -1,9 +0,0 @@
-#!//bin/bash
-# ttest move .bashrc file to git repository foldder then update using kmgite script
-
-# move the .bashrc file to ~/bashrc/ folder 
-cp ~/.bashrc ~/bashrc/.bashrc || exit 1
-
-cd ~/bashrc/
-
-kmgit.sh
