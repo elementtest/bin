d561fa41090fc2961a3ddf451275f6d138c7d30c
elementtest <elementtest8@gmail.com>
2026-01-02 18:47:48 +0800

    comment

--- grep.sh
+++ grep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# basic script to search all folders and subfolders for a string
 
 # Prompt user for search string
 read -p "Enter the string to search for (in this folder and all subfolders) : " search_string
