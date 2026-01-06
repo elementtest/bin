6f96e46d35fdbd6857ea444fba3a07d4eb1a7f5c
elementtest <elementtest8@gmail.com>
2026-01-06 22:45:24 +0800

    comment added

--- 0.sh
+++ 0.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# script to simple save a time stamp
+# if you use the script again it will update the time stamp
+# PURPOSE: to track how much time i spend studying instead of writing it down
 
 FILE="$HOME/.way-to-record-study-time"
 
