54d5244a71904c3ff2b0a8318fdc8aa8aefcbe2c
elementtest <elementtest8@gmail.com>
2026-03-04 16:05:42 +0800

    change htmk script (to generate html, css and js files) will now generate style.css instead of styleS

--- htmk.sh
+++ htmk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#script to make a new directory with index, style.css and script.js
+# script to make a directory (user asked for name) then touch html, css and js files
 read -p "Enter name of directory?  will generate index, css and js inside: then cd into and open index.html  " dirname
 
 mkdir "$dirname" || { echo "Failed to create directory"; exit 1; }
@@ -7,10 +7,10 @@ mkdir "$dirname" || { echo "Failed to create directory"; exit 1; }
 cd "$dirname" || { echo "Failed to change directory"; exit 1; }
 
 touch index.html
-touch styles.css
+touch style.css
 touch script.js
 
-nvim *
+ls
 
 
 
